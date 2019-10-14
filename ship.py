a3b401399fc500d6e4bfd0163b76b85788ea636f
Adrian Viquez <aviquez96@gmail.com>
2019-10-14 10:27:29 -0600

    Refactored ship's functionality

--- ship.py
+++ ship.py
@@ -10,22 +10,22 @@ class Ship():
         self.rectangle = self.image.get_rect()
         self.screen_rectangle = screen.get_rect()
 
+        self.rectangle.centerx = self.screen_rectangle.centerx
+        self.rectangle.bottom = self.screen_rectangle.bottom
+
         self.center = float(self.rectangle.centerx)
 
         self.moving_right = False
         self.moving_left = False
-
-        self.rectangle.centerx = self.screen_rectangle.centerx
-        self.rectangle.bottom = self.screen_rectangle.bottom
 
     def blitme(self):
         #draws the image at the position of rectangle
         self.screen.blit(self.image, self.rectangle)
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rectangle.right < self.screen_rectangle.right:
             self.center += self.ai_settings.ship_speed_factor
-        elif self.moving_left:
+        elif self.moving_left and self.rectangle.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
 
         self.rectangle.centerx = self.center

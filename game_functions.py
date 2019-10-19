4cb28072600baaccbcda65a7f229f772e6f8c446
Adrian Viquez <aviquez96@gmail.com>
2019-10-19 00:02:35 -0700

    Created an Alien fleet to display initially

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
@@ -35,13 +36,25 @@ def fire_bullet(ai_settings, screen, ship, bullets):
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def create_fleet(ai_settings, screen, aliens):
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+
+    for alien_number in range(number_aliens_x):
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
+
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     screen.fill(ai_settings.background_color)
     #redraw all bullets behind ship an aliens
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
 
     #Refresh the most recently drawn screen
     pygame.display.flip()

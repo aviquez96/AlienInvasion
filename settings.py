9035e0eadb741f30429cd7636404bb3bb72b0852
Adrian Viquez <aviquez96@gmail.com>
2019-10-23 15:14:08 -0700

    Dynamic bullet increase

--- settings.py
+++ settings.py
@@ -17,6 +17,7 @@ class Settings():
         self.fleet_drop_speed = 10
 
         self.speedup_scale = 1.1
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -27,8 +28,12 @@ class Settings():
         self.alien_points = 50
 
         self.fleet_direction = 1
-    
+
     def increase_speed(self):
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.score_scale * self.alien_points)
+
+        print(self.alien_points)
